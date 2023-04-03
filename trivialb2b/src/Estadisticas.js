c3a884f0fbc79eb25bebada2d3248b4e64d23b5f
Rober-Garcia <76645510+Rober-Garcia@users.noreply.github.com>
2023-04-03 20:45:22 +0200

    Partida casi completa a falta de comunicacion back

--- trivialb2b/src/Estadisticas.js
+++ trivialb2b/src/Estadisticas.js
@@ -8,57 +8,29 @@ const URL = "http://51.142.118.71:8000/api/usuarios/login/";
 
 
 const Estadisticas = () => {
-  const [body, setBody] = useState({ username: "", password: "" });
-  const [errores, setErorres] = useState("");
 
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setBody({
-      ...body,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const onSubmit = () => {
-    //console.log(body);
-    navigate(process.env.PUBLIC_URL+'/MenuJuego');
-    fetch(URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      //.then((data) => console.log(data))
-      .then((data) => {console.log(data)
-        if ((body.username=="") && (body.password=="")) {
-          setErorres("Vacio")
-        }
-        if (data.OK == "True"){
-          setErorres("");
-          const usuario = { nombre: body.username, contraseña: body.password};
-          setSession({usuario});
-          navigate(process.env.PUBLIC_URL+'/MenuPrincipal');
-        }
-        else {
-          if (data.error_username !== "") {
-            setErorres("Error name");
-          }
-          else if (data.error_password !== ""){
-            setErorres("Error password");
-          }
-        }
-      })
-      .catch((error) => console.error(error));
-  };
+    const [position, setPosition] = useState(0);
+  
+    const handleClick = () => {
+      // Mueve el componente 100px a la derecha
+      setPosition(position + 100);
+      
+      // Vuelve a la posición original después de 1 segundo
+      setTimeout(() => {
+        setPosition(position - 100);
+      }, 1000);
+    };
 
   return (
-    <div className="App">
-      <div className = "App-header" > 
-      <div className="App-titulo" > Estadisticas
-        <div className="App-Quesitos"> </div> 
-        </div>
-        </div>
-    </div>
+    <div onClick={handleClick}
+    style={{
+      position: "absolute",
+      left: `${position}px`,
+      width: "50px",
+      height: "50px",
+      backgroundColor: "red"
+    }}
+  />
   );
 };
 
