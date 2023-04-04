de4fa763fda3a0a945d8e22b5a730a20f336a67d
Rober-Garcia <76645510+Rober-Garcia@users.noreply.github.com>
2023-04-04 13:11:05 +0200

    Tienda

--- trivialb2b/src/Estadisticas.js
+++ trivialb2b/src/Estadisticas.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef} from "react";
 import './Estilos/App.css';
 import { useNavigate } from 'react-router-dom';
 import { useSession, setSession } from 'react-session';
@@ -7,30 +7,44 @@ import { useSession, setSession } from 'react-session';
 const URL = "http://51.142.118.71:8000/api/usuarios/login/";
 
 
-const Estadisticas = () => {
 
-    const [position, setPosition] = useState(0);
-  
-    const handleClick = () => {
-      // Mueve el componente 100px a la derecha
-      setPosition(position + 100);
-      
-      // Vuelve a la posición original después de 1 segundo
-      setTimeout(() => {
-        setPosition(position - 100);
-      }, 1000);
+const Estadisticas = () => {
+  const data = [
+    { id: 1, title: 'Elemento 1', image: 'https://via.placeholder.com/150' },
+    { id: 2, title: 'Elemento 2', image: 'https://via.placeholder.com/150' },
+    { id: 3, title: 'Elemento 3', image: 'https://via.placeholder.com/150' },
+    { id: 4, title: 'Elemento 4', image: 'https://via.placeholder.com/150' },    { id: 1, title: 'Elemento 1', image: 'https://via.placeholder.com/150' },
+    { id: 2, title: 'Elemento 2', image: 'https://via.placeholder.com/150' },
+    { id: 3, title: 'Elemento 3', image: 'https://via.placeholder.com/150' },
+    { id: 4, title: 'Elemento 4', image: 'https://via.placeholder.com/150' },    { id: 1, title: 'Elemento 1', image: 'https://via.placeholder.com/150' },
+    { id: 2, title: 'Elemento 2', image: 'https://via.placeholder.com/150' },
+    { id: 3, title: 'Elemento 3', image: 'https://via.placeholder.com/150' },
+    { id: 4, title: 'Elemento 4', image: 'https://via.placeholder.com/150' }]
+  const [visibleItems, setVisibleItems] = useState(4);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current;
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+        setVisibleItems(visibleItems + 4);
+      }
     };
+    containerRef.current.addEventListener('scroll', handleScroll);
+    return () => {
+      containerRef.current.removeEventListener('scroll', handleScroll);
+    };
+  }, [visibleItems])
 
   return (
-    <div onClick={handleClick}
-    style={{
-      position: "absolute",
-      left: `${position}px`,
-      width: "50px",
-      height: "50px",
-      backgroundColor: "red"
-    }}
-  />
+    <div className="horizontal-list" ref={containerRef}>
+      {data.slice(0, visibleItems).map((item) => (
+        <div key={item.id} className="horizontal-list__item">
+          <img src={item.image} alt={item.title} />
+          <h2>{item.title}</h2>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- trivialb2b/src/Tienda.js
+++ trivialb2b/src/Tienda.js
@@ -1,127 +1,128 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './Estilos/Estilo.css';
 import { useNavigate } from 'react-router-dom';
+import Cristiano from'./Imagenes/Cristiano.jpg';
+import Moneda from'./Imagenes/Moneda.png';
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 //const URL = "https://6e01-146-158-156-138.eu.ngrok.io/api/usuarios/login/";
 const URL = "https://51.142.118.71:8000/api/usuarios/login/";
 
 
 const Tienda = () => {
-  const [cubeStyle, setCubeStyle] = useState({
-    transform: 'translateY(400px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
-  });
-  const time = 4;
 
-  const f1 = () => {
-    setCubeStyle({
-      ...cubeStyle,
-      transition: '',
-      transform: 'translateY(400px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)'
-    });
+  const [show, setShow] = useState(false);
 
-    setTimeout(() => {
-      setCubeStyle({
-        ...cubeStyle,
-        transition: `transform ${time}s`
-      });
+  const tableroSeleccionado = {nombre:"Tablero1", imagen:Cristiano, valor:"10"}
 
-      const randomValue = Math.floor((Math.random() * 6) + 1);
-      console.log(`randomValue: ${randomValue}`);
+  const tableros = [{nombre:"Tablero1", imagen:Cristiano},{nombre:"Tablero2", imagen:Cristiano},{nombre:"Tablero3", imagen:Cristiano},{nombre:"Tablero4", imagen:Cristiano},{nombre:"Tablero5", imagen:Cristiano},{nombre:"Tablero6", imagen:Cristiano}];
 
-      switch(randomValue) {
-        case 1:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)'
-          });
-          break;
-        case 2:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)'
-          });
-          break;
-        case 3:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)'
-          });
-          break;
-        case 4:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)'
-          });
-          break;
-        case 5:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)'
-          });
-          break;
-        case 6:
-          setCubeStyle({
-            ...cubeStyle,
-            transform: 'translateY(400px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)'
-          });
-          break;
-      };
-    }, time * 10);
-  };
+  const fichas = [{nombre:"Ficha1", imagen:Cristiano},{nombre:"Ficha2", imagen:Cristiano},{nombre:"Ficha3", imagen:Cristiano},{nombre:"Ficha4", imagen:Cristiano},{nombre:"Ficha5", imagen:Cristiano},{nombre:"Ficha6", imagen:Cristiano}];
 
+  const [visibleItems, setVisibleItems] = useState(4);
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current;
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth) {
+        setVisibleItems(visibleItems + 4);
+      }
+    };
+    containerRef.current.addEventListener('scroll', handleScroll);
+    return () => {
+      containerRef.current.removeEventListener('scroll', handleScroll);
+    };
+  }, [visibleItems])
+  
+  function Tablero() {
+    return (
+      <div className="horizontal-list" ref={containerRef}>
+      {tableros.slice(0, visibleItems).map((item) => (
+        <div key={item.nombre} className="horizontal-list__item">
+          <div style={{border: "2px solid white", height:"97.9%"}} >
+            <div style={{ height:"20%", width:"100%", alignItems:"center"}}>
+              <a style={{color:"white", fontSize:"20px"}}>
+                {item.nombre}
+              </a>
+            </div>
+            <img src={item.imagen} className="App-imagenJugador"  style= {{ width:"100px", height:"100px", position:"relative", top:"10%"}}/>
+          </div>
+        </div>
+      ))}
+      </div>
+    )
+  }
 
+  function Ficha() {
+    return (
+      <div className="horizontal-list" ref={containerRef}>
+      {fichas.slice(0, visibleItems).map((item) => (
+        <div key={item.nombre} className="horizontal-list__item">
+          <div style={{border: "2px solid white", height:"97.9%"}} >
+            <div style={{ height:"20%", width:"100%", alignItems:"center"}}>
+              <a style={{color:"white", fontSize:"20px"}}>
+                {item.nombre}
+              </a>
+            </div>
+            <img src={item.imagen} className="App-imagenJugador"  style= {{ width:"100px", height:"100px", position:"relative", top:"10%"}}/>
+          </div>
+        </div>
+      ))}
+      </div>
+    )
+  }
 
   return (
-<div class="container">
-        <div class="cube" onClick={() => f1()}  style={{transform:cubeStyle.transform ,transition:cubeStyle.transition}}>
-            <div class="cube-face front">
-                <div class="inside">
-                    <span class="dot"></span>
-                </div>
+    <div className="App">
+      <div className = "App-header" > 
+          <div className="App-titulo" style= {{top: "7%"}} > Tienda
+              <div className="App-Quesitos" style= {{left: "30%"}}> </div> 
+          </div>
+      </div>
+
+
+      <div className="App-CuadradoNegro"  style= {{width:"65%", height:"30%", top: "26%", left: "7%", position:"absolute", border: "2px solid white", borderRadius: "50px 50px 0px 0px"}}>
+          <div className="App-CuadradoNegro"  style= {{width:"100%", height:"30%", top: "0%", left: "-0.2%", position:"relative", border: "2px solid white", borderRadius: "50px 50px 0px 0px"}}>
+            <div style={{marginTop:"1%"}}>
+              <a style={{color:"white",fontSize:"40px"}}>
+                    Tablero
+              </a>
             </div>
-            <div class="cube-face back">
-                <div class="inside">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
-            </div>
-            <div class="cube-face left">
-                <div class="inside">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
-            </div>
-            <div class="cube-face right">
-                <div class="inside">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
-            </div>
-            <div class="cube-face top">
-                <div class="inside">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
-            </div>
-            <div class="cube-face bottom">
-                <div class="inside">
-                    <span class="dot"></span>
-                    <span class="dot"></span>
-                </div>
-            </div>
+          </div>
+
+          <Tablero/>
+
         </div>
+
+      <div className="App-CuadradoNegro"  style= {{width:"65%", height:"30%", top: "60%", left: "7%", position:"absolute", border: "2px solid white", borderRadius: "50px 50px 0px 0px"}}>
+        <div className="App-CuadradoNegro"  style= {{width:"100%", height:"30%", top: "0%", left: "-0.2%", position:"relative", border: "2px solid white", borderRadius: "50px 50px 0px 0px"}}>
+          <div style={{marginTop:"1%"}}>
+            <a style={{color:"white",fontSize:"40px"}}>
+                  Fichas
+            </a>
+          </div>
+        </div>
+
+        <Ficha/>
+      </div>
+      <div style={{position:"absolute", top:"15%", left:"80%"}}>
+      <a style={{color:"white",fontSize:"40px", fontStyle: "italic"}}>Monedas: 18</a>
+      </div>
+      <img src={Moneda} className="App-imagenJugador"  style= {{ width:"300px", height:"300px", position:"absolute", top:"20%", left:"78%"}}/>
+
+
+      {show ? ( 
+        <div>
+
+        </div>
+      ) : (
+        <div style={{position:"absolute", top:"15%", left:"80%"}}>
+          <a style={{color:"white",fontSize:"40px", fontStyle: "italic"}}>El tablero {tableroSeleccionado.nombre} cuesta: {tableroSeleccionado.valor}</a>
+        </div>
+      )}
+
+
     </div>
-    
   );
 };
 
