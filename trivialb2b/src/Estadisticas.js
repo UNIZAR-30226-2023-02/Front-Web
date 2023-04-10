eeb7c242a066a97772ff67bfb5fe112645e02f88
Rober-Garcia <76645510+Rober-Garcia@users.noreply.github.com>
2023-04-10 10:54:24 +0200

    Guardado empezando estadisticas

--- trivialb2b/src/Estadisticas.js
+++ trivialb2b/src/Estadisticas.js
@@ -2,86 +2,59 @@ import React, { useState, useEffect, useRef} from "react";
 import './Estilos/App.css';
 import { useNavigate } from 'react-router-dom';
 import { useSession, setSession } from 'react-session';
+import Atras from "./Imagenes/Atras.png";
 
 //const URL = "https://6e01-146-158-156-138.eu.ngrok.io/api/usuarios/login/";
 const URL = "http://51.142.118.71:8000/api/usuarios/login/";
 
   const Estadisticas = () => {
-    const roomName= "pepe3";
-    const [chatLog, setChatLog] = useState('');
-    const [messageInput, setMessageInput] = useState('');
-    const chatLogRef = useRef(null);
-    const chatSocketRef = useRef(null);
-  
-    useEffect(() => {
-      chatSocketRef.current = new WebSocket(
-        `ws://b64b-146-158-156-138.eu.ngrok.io/ws/chat/${roomName}/`
-      );
-  
-      chatSocketRef.current.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-        setChatLog(prevChatLog => prevChatLog + data.message + '\n');
-      };
+    const navigate = useNavigate();
+    const flechaAtras = async () => {
+      navigate(process.env.PUBLIC_URL+ '/MenuJuego');
+    };
 
-      chatSocketRef.current.onerror = function(event) {
-        console.error('Chat socket error:', event);
-      };
-      
-  
-      chatSocketRef.current.onclose = function(event) {
-        console.error('Chat socket closed unexpectedly');
-      };
-  
-      return () => {
-        chatSocketRef.current.close();
-      };
-    }, [roomName]);
-  
-    useEffect(() => {
-      chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight;
-    }, [chatLog]);
-  
-    function handleMessageInputChange(event) {
-      setMessageInput(event.target.value);
-    }
-  
-    function handleChatMessageSubmit() {
-      const message = messageInput.trim();
-      if (message) {
-        chatSocketRef.current.send(JSON.stringify({ message }));
-        setMessageInput('');
-      }
-    }
-  
-    function handleKeyPress(event) {
-      if (event.key === 'Enter') {
-        handleChatMessageSubmit();
-      }
-    }
+
   
     return (
-      <div style={{position:"absolute", top:"0%", left:"70%", width:"29.6%", height:"100%"}}>
-        <textarea
-          style={{position:"absolute", top:"0%", left:"0%", width:"100%", height:"90%"}}
-          ref={chatLogRef}
-          id="chat-log"
-          cols="100"
-          rows="20"
-          value={chatLog}
-          readOnly
-        />
-        <input
-          id="chat-message-input"
-          type="text"
-          size="100"
-          value={messageInput}
-          onChange={handleMessageInputChange}
-          onKeyPress={handleKeyPress}
-          style={{position:"absolute", top:"90%", left:"0%", width:"100%", height:"9%", border:" 2px solid black", borderRadius:"0px 0px 30px 30px"}}
-        />
-        <button id="chat-message-submit" onClick={handleChatMessageSubmit} className="App-botonSinS" style={{position:"absolute", top:"90.2%", left:"76%", fontSize:"30px", width:"25%", height:"9.3%", borderRadius:"50px 0px 30px 50px"}}>
-          Enviar
-        </button>
+      <div className="App">
+        <div className="App-CuadradoNegro" style={{ width: "850px", height: "600px", position: "absolute", top: "25%", left: "5%", border: "2px solid white"}}>
+
+          <div className="App-CuadradoNegro" style={{ width: "45%", height: "25%", position: "absolute", top: "5%", left: "2%", border: "2px solid white"}}>
+
+            <div className="App-CuadradoNegro" style={{ width: "99%", height: "20%", position: "absolute", top: "-1.5%", left: "0%", borderRadius: "100px 100px 0px 0px", border: "2px solid white",}}>
+
+            </div>
+            <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "25%"}}>
+              pepe
+            </a>
+        
+          </div>
+
+        </div>
+
+
+        <div className="App-CuadradoNegro" style={{ width: "850px", height: "600px", position: "absolute", top: "25%", left: "50%", border: "2px solid white"}}>
+
+          <div className="App-CuadradoNegro" style={{ width: "50%", height: "88%", position: "absolute", top: "5%", left: "2%", borderRadius: "50px 0px 0px 50px", border: "2px solid white"}}>
+
+            <div className="App-CuadradoNegro" style={{ width: "50%", height: "88%", position: "absolute", top: "5%", left: "2%", borderRadius: "50px 0px 0px 50px", border: "2px solid white"}}>
+
+            </div>
+            <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "25%"}}>
+              pepe
+            </a>
+        
+          </div>
+
+        </div>
+        <img src={Atras} style={{width:"150px", height:"150px", top:"5%", left:"5%", cursor: "pointer", position: "absolute"}} onClick={() => flechaAtras()}/>
+        <div className = "App-header" > 
+            <div className="App-titulo" style= {{top: "7%"}} > Estadisticas
+                <div className="App-Quesitos" style= {{left: "35%"}}> </div> 
+            </div>
+        </div>
+
+        
       </div>
     );
   };
