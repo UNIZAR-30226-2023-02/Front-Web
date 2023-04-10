4301acf4e3a1f6712ddec017fc0b2a786f353b0c
Acheregido <71550237+Acheregido@users.noreply.github.com>
2023-04-10 17:35:19 +0200

    Co-authored-by: Rober-Garcia <Rober-Garcia@users.noreply.github.com>

--- trivialb2b/src/Registrarse.js
+++ trivialb2b/src/Registrarse.js
@@ -12,10 +12,11 @@ function CuadroTexto(props) {
     <div>
       <a> {props.texto}:  </a>
       <input className="App-textoNegro"
+        type={props.type}
         label={props.label}
         name={props.nombre}
         value={props.valor}
-        onChange={props.funcion}
+        onChange={props.accion}
       />
     </div>
   )
@@ -23,8 +24,9 @@ function CuadroTexto(props) {
 
 
 const Registrarse = () => {
-  const [body, setBody] = useState({ username: "Pepe", password: "123456789", confirm_password: "123456789", fecha_nac: "1111-11-11", correo: "pepe@gmail.com", telefono:"123456789"});
+  const [body, setBody] = useState({ username: "", password: "", confirm_password: "", fecha_nac: "aaaa-mm-dd", correo: "", telefono:""});
   const [errores, setErorres] = useState("");
+  const [usuario, setUsuario] = useState();
 
   const navigate = useNavigate();
   
@@ -41,8 +43,6 @@ const Registrarse = () => {
 
 
   const Registrarse = () => {
-    console.log(body);
-    //navigate(process.env.PUBLIC_URL + '/MenuJuego');
     
     fetch(URL, {
       method: "POST",
@@ -50,29 +50,33 @@ const Registrarse = () => {
       body: JSON.stringify(body),
     })
       .then((response) => response.json())
-      //.then((data) => console.log(data))
+      .then((data) => {console.log(data)
 
-      .then((data) => { 
-        console.log(data)
-        if (data.OK == "True"){
+        if ((body.username="")&&(body.password="") && (body.confirm_password="") && (body.fecha_nac="") && (body.correo="") && (body.telefono="")) {
+          setErorres("Algún campo esta vacío");
+        }
+        else if (data.OK == "True"){
           setErorres("");
           navigate(process.env.PUBLIC_URL+'/MenuJuego');
         }
         else {
           if (data.error_username !== "") {
-            setErorres("Error name");
+            setErorres(data.error_username);
           }
           else if (data.error_password !== ""){
-            setErorres("Error password");
+            setErorres(data.error_password);
           }
           else if (data.error_confirm_password !== ""){
-            setErorres("Error confirm password");
+            setErorres(data.error_confirm_password);
           }
           else if (data.error_fecha_nac !== ""){
-            setErorres("Error fecha nac");
+            setErorres(data.error_fecha_nac);
           }
           else if (data.error_correo !== ""){
-            setErorres("Error correo");
+            setErorres(data.error_correo);
+          }
+          else if (data.error_telefono !== "") {
+            setErorres(data.error_telefono);
           }
         }
       })
@@ -90,20 +94,26 @@ const Registrarse = () => {
           <form className="App-Input" style={{left: "10%", top:"45%", height:"30%", width: "83%", position: "absolute"}}>
             <div className="App-CuadrosTextoIzq" > 
               <div style={{marginLeft:"6.9%"}}>
-                <CuadroTexto texto="Usuario" label="username" nombre="username" valor={body.username} funcion={handleChange} />
+                <CuadroTexto texto="Usuario" type="username" label="username" nombre="username" valor={body.username} accion={handleChange} />
               </div>
-              <CuadroTexto texto="Contraseña" type="password" label="password" nombre="password"  valor={body.password} funcion={handleChange} />
+              <CuadroTexto texto="Contraseña" type="password" label="password" nombre="password"  valor={body.password} accion={handleChange} />
               <div style={{marginLeft:"2.8%"}}>
-                <CuadroTexto texto="Confirmar" type="password" label="confpassword" nombre="confirm_password"  valor={body.confirm_password} funcion={handleChange} />
+                <CuadroTexto texto="Confirmar" type="password" label="confpassword" nombre="confirm_password"  valor={body.confirm_password} accion={handleChange} />
               </div>
             </div>
             <div className="App-CuadrosTextoDer" > 
-              <CuadroTexto texto="Fecha de nacimiento" label="fecha_nac" nombre="fecha_nac"  valor={body.fecha_nac} funcion={handleChange} /> 
+              <CuadroTexto texto="Fecha de nacimiento" type="fecha_nac" label="fecha_nac" nombre="fecha_nac"  valor={body.fecha_nac} accion={handleChange} /> 
               <div style={{marginLeft:"3%"}}>
-                <CuadroTexto texto="Correo electronico" type="email" label="correo" nombre="correo" valor={body.correo} funcion={handleChange} />
+                <CuadroTexto texto="Correo electronico" type="email" label="correo" nombre="correo" valor={body.correo} accion={handleChange} />
+              </div>
+              <div style={{marginLeft:"20%"}}>
+                <CuadroTexto texto="Telefono" type="telefono" label="telefono" nombre="telefono"  valor={body.telefono} accion={handleChange} />
               </div>
             </div>
           </form>
+          <div style={{top:"62%", position:"absolute", fontSize:"25px"}}>
+            <font color="red" > {errores}</font>
+          </div>
           <button className="App-boton" style= {{top: "80%", left: "auto", position:"absolute"}} onClick={() => Registrarse()} >
             Registrarse
           </button>
