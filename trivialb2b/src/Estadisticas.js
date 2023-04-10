8c97fa11520113d7cd6f6d4ecc83a54e367adf3e
Rober-Garcia <76645510+Rober-Garcia@users.noreply.github.com>
2023-04-10 17:55:31 +0200

    Estadisticas Histirial

--- trivialb2b/src/Estadisticas.js
+++ trivialb2b/src/Estadisticas.js
@@ -8,42 +8,57 @@ import Atras from "./Imagenes/Atras.png";
 const URL = "http://51.142.118.71:8000/api/usuarios/login/";
 
   const Estadisticas = () => {
+
     const navigate = useNavigate();
     const flechaAtras = async () => {
       navigate(process.env.PUBLIC_URL+ '/MenuJuego');
     };
 
+    function Tema(props) {
+      return (    
+      <div className="App-CuadradoNegro" style={{ width: "45%", height: "25%", position: "absolute", top: props.top, left: props.left, border: "2px solid white"}}>  
+        <div className="App-CuadradoNegro" style={{ width: "99%", height: "25%", position: "absolute", top: "-1.5%", left: "0%", borderRadius: "100px 100px 0px 0px", border: "2px solid white", backgroundColor:props.color, textAlign:"center"}}>
+          <a  style= {{ color: "white", fontSize: "30px", fontStyle: "italic"}}> {props.tema} </a>
+        </div>
+        <a  style= {{ color: "white", fontSize: "20px", fontStyle: "italic" ,position: "absolute", top: "30%", left: "10%"}}> Respuestas <font color="green">acertadas</font>: {props.aciertos} </a>
 
+        <a  style= {{ color: "white", fontSize: "20px", fontStyle: "italic" ,position: "absolute", top: "50%", left: "10%"}}> Respuestas <font color="red">falladas</font>: {props.fallos} </a>
+        <br></br>
+        <a  style= {{ color: "white", fontSize: "20px", fontStyle: "italic" ,position: "absolute", top: "70%", left: "15%"}}> Porcentajes de acierto: {props.porcentaje} </a>
+      </div>
+      );
+    }
   
     return (
       <div className="App">
         <div className="App-CuadradoNegro" style={{ width: "850px", height: "600px", position: "absolute", top: "25%", left: "5%", border: "2px solid white"}}>
 
-          <div className="App-CuadradoNegro" style={{ width: "45%", height: "25%", position: "absolute", top: "5%", left: "2%", border: "2px solid white"}}>
 
-            <div className="App-CuadradoNegro" style={{ width: "99%", height: "20%", position: "absolute", top: "-1.5%", left: "0%", borderRadius: "100px 100px 0px 0px", border: "2px solid white",}}>
-
-            </div>
-            <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "25%"}}>
-              pepe
-            </a>
+          <Tema top="5%" left="2%" tema="Geografia" color="blue" aciertos="30" fallos="30" porcentaje="50%"/>
+          <Tema top="5%" left="52%" tema="Ciencia y naturaleza" color="green" aciertos="20" fallos="20" porcentaje="50%"/>
+          <Tema top="35%" left="2%" tema="Historia" color="#a0a856" aciertos="40" fallos="40" porcentaje="50%"/>
+          <Tema top="35%" left="52%" tema="Entrtenimiento" color="rgb(226, 4, 255)" aciertos="70" fallos="70" porcentaje="50%"/>
+          <Tema top="65%" left="2%" tema="Deporte" color="rgb(236, 134, 1)" aciertos="70" fallos="70" porcentaje="50%"/>
+          <Tema top="65%" left="52%" tema="Arte y Literatura" color="Red" aciertos="80" fallos="80" porcentaje="50%"/>
+          
         
-          </div>
 
         </div>
 
 
         <div className="App-CuadradoNegro" style={{ width: "850px", height: "600px", position: "absolute", top: "25%", left: "50%", border: "2px solid white"}}>
 
-          <div className="App-CuadradoNegro" style={{ width: "50%", height: "88%", position: "absolute", top: "5%", left: "2%", borderRadius: "50px 0px 0px 50px", border: "2px solid white"}}>
-
-            <div className="App-CuadradoNegro" style={{ width: "50%", height: "88%", position: "absolute", top: "5%", left: "2%", borderRadius: "50px 0px 0px 50px", border: "2px solid white"}}>
-
-            </div>
-            <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "25%"}}>
-              pepe
-            </a>
-        
+          <div className="App-CuadradoNegro" style={{ width: "100%", height: "50%", position: "absolute", top: "-0.3%", left: "-0.2%", borderRadius: "50px 50px 0px 0px", border: "2px solid white", textAlign:"center"}}>
+            <a  style= {{ color: "white", fontSize: "50px", fontStyle: "italic"}}> Total de preguntas <br></br>100 </a>
+          </div>
+          <div className="App-CuadradoNegro" style={{ width: "260px", height: "260px", position: "absolute", top: "27%", left: "35%", borderRadius: "50%", border: "2px solid white", backgroundColor:"gray", zIndex:"1"}}>
+            <a  style= {{ color: "white", fontSize: "50px", fontStyle: "italic" , position: "absolute", top: "25%", left: "17%"}}> Aciertos <br></br>50% </a>
+          </div>
+          <div className="App-CuadradoNegro" style={{ width: "50%", height: "50%", position: "absolute", top: "50%", left: "-0.2%", borderRadius: "0px 0px 0px 50px", border: "2px solid white", backgroundColor:"green"}}>
+            <a  style= {{ color: "white", fontSize: "40px", fontStyle: "italic", position: "absolute", top: "30%", left: "-5%"}}> Total de respuestas correctas <br></br>50 </a>
+          </div>
+          <div className="App-CuadradoNegro" style={{ width: "50%", height: "50%", position: "absolute", top: "50%", left: "50%", borderRadius: "0px 0px 50px 0px", border: "2px solid white", backgroundColor:"red"}}>
+          <a  style= {{ color: "white", fontSize: "40px", fontStyle: "italic", position: "absolute", top: "30%", left: "15%"}}> Total de respuestas incorrectas <br></br>50 </a>
           </div>
 
         </div>
--- trivialb2b/src/Historial.js
+++ trivialb2b/src/Historial.js
@@ -2,58 +2,114 @@ import React, { useState } from "react";
 import './Estilos/App.css';
 import { useNavigate } from 'react-router-dom';
 import { useSession, setSession } from 'react-session';
+import Atras from "./Imagenes/Atras.png";
+import Vinicius from "./Imagenes/Vinicius.png";
 
 //const URL = "https://6e01-146-158-156-138.eu.ngrok.io/api/usuarios/login/";
 const URL = "http://51.142.118.71:8000/api/usuarios/login/";
 
 
 const Historial = () => {
-  const [body, setBody] = useState({ username: "", password: "" });
-  const [errores, setErorres] = useState("");
+
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
 
   const navigate = useNavigate();
-  const handleChange = (e) => {
-    setBody({
-      ...body,
-      [e.target.name]: e.target.value,
-    });
+  const flechaAtras = async () => {
+    navigate(process.env.PUBLIC_URL+ '/MenuJuego');
+  };
+  const clasico = async () => {
+    setShow1(true);
+  };
+  const equipos = async () => {
+    setShow2(true);
+  };
+  const tematica = async () => {
+    setShow3(true);
   };
 
-  const onSubmit = () => {
-    //console.log(body);
-    navigate(process.env.PUBLIC_URL+'/MenuJuego');
-    fetch(URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      //.then((data) => console.log(data))
-      .then((data) => {console.log(data)
-        if ((body.username=="") && (body.password=="")) {
-          setErorres("Vacio")
-        }
-        if (data.OK == "True"){
-          setErorres("");
-          const usuario = { nombre: body.username, contraseña: body.password};
-          setSession({usuario});
-          navigate(process.env.PUBLIC_URL+'/MenuPrincipal');
-        }
-        else {
-          if (data.error_username !== "") {
-            setErorres("Error name");
-          }
-          else if (data.error_password !== ""){
-            setErorres("Error password");
-          }
-        }
-      })
-      .catch((error) => console.error(error));
-  };
+  function Partida(props) {
+    return (    
+      <div style={{ width: "800px", height: "100px", position: "relative", border: "2px solid white", backgroundColor:props.color, borderRadius:"50px", marginTop:"20px", cursor:"pointer"}} onClick={props.function}>
+        <img src={props.src} style={{width:"14%", height:"90%", top:"5%", left:"5%", cursor: "pointer", position: "absolute"}}/>
+        <a  style= {{ color: "white", fontSize: "30px", fontStyle: "italic", top:"20%", left:"40%", position: "absolute"}}> {props.modo} </a>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
+  <div className="App">
 
+
+    <div style={{ width: "45%", height: "65%", position: "absolute", top: "23%", left: "5%",  borderRadius:"50px"}}>
+      <Partida src={Vinicius} modo="Modo Clasico" color="green" function={clasico}/>
+      <Partida src={Vinicius} modo="Modo Equipos" color="red" function={equipos}/>
+      <Partida src={Vinicius} modo="Modo Clasico" color="green"/>
+      <Partida src={Vinicius} modo="Modo Clasico" color="red" />
+      <Partida src={Vinicius} modo="Modo Equipos" color="red" />
     </div>
+
+    <div style={{ width: "45%", height: "65%", position: "absolute", top: "23%", left: "53%", borderRadius:"50px"}}>
+      <Partida src={Vinicius} modo="Modo Tematica" color="blue" function={tematica}/>
+      <Partida src={Vinicius} modo="Modo Tematica" color="blue" />
+      <Partida src={Vinicius} modo="Modo Clasico" color="green" />
+      <Partida src={Vinicius} modo="Modo Equipos" color="green" />
+      <Partida src={Vinicius} modo="Modo Clasico" color="green" />
+    </div>
+
+
+    {show1 ? (
+      <div>
+        <a  style= {{ color: "white", fontSize: "40px", fontStyle: "italic", top:"17%", left:"20%", position: "absolute"}}> Modo Clasico </a>
+        <button className="App-boton" style= {{position: "absolute", top: "10%", left: "80%" }} onClick={() => { setShow1(false)}}>Volver </button>
+        <div className="App-CuadradoNegro" style={{ width: "90.5%", height: "68%", position: "absolute", top: "23%", left: "5%",border: "2px solid white"}}>
+
+        </div>
+      </div>
+    ) : (
+      <div/>
+
+    )}
+
+    {show2 ? (
+      <div>
+        <a  style= {{ color: "white", fontSize: "40px", fontStyle: "italic", top:"17%", left:"20%", position: "absolute"}}> Modo Equipos </a>
+        <button className="App-boton" style= {{position: "absolute", top: "10%", left: "80%" }} onClick={() => { setShow2(false)}}>Volver </button>
+        <div className="App-CuadradoNegro" style={{ width: "90.5%", height: "68%", position: "absolute", top: "23%", left: "5%",border: "2px solid white"}}>
+
+        </div>
+      </div>
+    ) : (
+      <div/>
+
+    )}
+
+    {show3 ? (
+      <div>
+        <a  style= {{ color: "white", fontSize: "40px", fontStyle: "italic", top:"17%", left:"20%", position: "absolute"}}> Modo Tematica </a>
+        <button className="App-boton" style= {{position: "absolute", top: "10%", left: "80%" }} onClick={() => { setShow2(false)}}>Volver </button>
+        <div className="App-CuadradoNegro" style={{ width: "90.5%", height: "68%", position: "absolute", top: "23%", left: "5%",border: "2px solid white"}}>
+
+        </div>
+      </div>
+    ) : (
+      <div/>
+
+    )}
+
+
+
+
+    <img src={Atras} style={{width:"150px", height:"150px", top:"5%", left:"5%", cursor: "pointer", position: "absolute"}} onClick={() => flechaAtras()}/>
+    <div className = "App-header" > 
+        <div className="App-titulo" style= {{top: "7%"}} > Historial
+            <div className="App-Quesitos" style= {{left: "35%"}}> </div> 
+        </div>
+    </div>
+
+    
+  </div>
   );
 };
 
