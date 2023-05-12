e789b5973b49d63f95b04de17640c2246b7ef10a
Rober-Garcia <76645510+Rober-Garcia@users.noreply.github.com>
2023-05-12 21:12:03 +0200

    Peticiones y boton unirse a partida activa

--- trivialb2b/src/BuscarPartida.js
+++ trivialb2b/src/BuscarPartida.js
@@ -9,12 +9,14 @@ import Cookies from 'universal-cookie';
 
 const URL = "http://51.142.118.71:8000/api/salas/lista-salas/";
 const URL2 = "http://51.142.118.71:8000/api/salas/validar/";
+const URL3 = "http://51.142.118.71:8000/api/partida/activa/";
 const URL1 = "http://51.142.118.71:8000/api/salas/listar-peticiones-sala/";
 
 const ModoClasico = () => {
   const [contraseña, setContraseña] = useState("");
 
   const [sala, setSala] = useState({ nombre: "", tiempo: "", numJugadores: "", password: "", creador: "", modo: "", tipo: ""});
+  const [pet, setPet] = useState({ me_invita: "", ws: "" });
   const [peticiones, setPeticiones] = useState([]);
   const [salas, setSalas] = useState([]);
 
@@ -34,6 +36,8 @@ const ModoClasico = () => {
   const [show, setShow] = useState(true);
   const [show1, setShow1] = useState(true);
   const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
 
   
   const navigate = useNavigate();
@@ -55,8 +59,22 @@ const ModoClasico = () => {
         })
         setSearchResults(salas);
         setSalas(salas)
-        
         setShow2(true)
+        
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+
+    fetch(URL3, {
+      method: "POST",
+      headers: {"Authorization": "Token " + token, "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {console.log(data)
+        if (data.OK == "True") {
+          setShow4(true)
+        }
         
     })
     .catch((error) => {
@@ -76,6 +94,14 @@ const ModoClasico = () => {
     })
   };  
 
+  const handleChange2 = (e) => {
+    setPet({
+      ...pet,
+      ["me_invita"]: e.me_invita,
+      ["ws"]: e.ws,
+    })
+  };  
+
 
   const cancelar = async (event) => {
     navigate(process.env.PUBLIC_URL + '/MenuJuego');
@@ -90,10 +116,14 @@ const ModoClasico = () => {
       .then((response) => response.json())
       .then((data) => {console.log(data)
         if (data.OK = "true"){
+          console.log(peticiones)
+          peticiones.length = 0
+          console.log(peticiones)
           data.peticiones.forEach(element => {
             peticiones.push(element)
           })
           setPeticiones(peticiones)
+          setShow2(false)
           console.log(peticiones)
         }
  
@@ -103,21 +133,11 @@ const ModoClasico = () => {
     });
   };  
 
-  /*const handleChange4 = (e) => {
-    setContraseña({
-      ...contraseña,
-      [e.target.name]: e.target.value,
-    });
-  };*/
-
   const handleChange4 = (e) => {
     setContraseña(e.target.value);
   };
 
   function funcionEntrar() {
-    console.log("FuncionEntrar");
-    //Entrar(sala.nombre, usuario, contraseña);
-    console.log(sala.nombre, usuario, contraseña);
     fetch(URL2, {
       method: "POST",
       headers: { "Authorization": "Token " + token, "Content-Type": "application/json" },
@@ -142,6 +162,12 @@ const ModoClasico = () => {
     });
   }
 
+  function funcionEntrar2() {
+    cookies.set('n_jugadores', pet.me_invita, {path: '/'})  ///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+    cookies.set('noCreador', 1, {path: '/'})
+    cookies.set('WebSocketEsperando', pet.ws, {path: '/'})
+    navigate(process.env.PUBLIC_URL+'/EsperandoJugadores');
+  }
 
   function jugadores() {
 
@@ -168,6 +194,29 @@ const ModoClasico = () => {
       </div>
     ));
   }
+
+
+  function invitaciones_lista() {
+
+    return peticiones.map(elemento  => (
+      <div className="App-CuadradoBlanco" style={{ width: "99.6%", height: "15%", position: "relative", left: "0%",cursor: "pointer"}}onClick={() => {handleChange2(elemento);setShow3(true)}}>
+        <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "5%"}}>
+          {elemento.me_invita}
+        </a>
+        <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "25%"}}>
+          {elemento.ws}
+        </a>
+        <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "45%"}}>
+          {elemento.me_invita}
+        </a>
+        <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "15%", left: "70%"}}>
+          {elemento.me_invita}
+        </a>
+      </div>
+    ));
+  }
+
+
   return (
     <div className="App">
       <div className = "App-header" > 
@@ -179,48 +228,117 @@ const ModoClasico = () => {
             </a>
           </div>
           <div>
-            <img src={Buscar} style={{width:"5%", height:"7%", left:"29%",zIndex: "1", top:"3%", cursor: "pointer", position:"absolute"}} /*onClick={search}*//>
-            <input className="App-textoNegro" label="pepe" name="pepe" onChange={handleChange3} value={searchTerm} style= {{top: "2%", left: "2%", position:"absolute", width: "500px", height: "50px"}} />;
-            <button className="App-botonAzul" style= {{top: "2%", left: "74%" , position:"absolute"}} onClick={() => invitaciones()} >
-              Invitaciones
+            
+          {show4 ? (
+            <button className="App-botonAzul" style= {{top: "2%", left: "60%" , position:"absolute", fontSize:"10px"}} onClick={() => cancelar()} >
+              Partida Activa
             </button>
+          ) : (
+            <div/>
+          )}
+
+
+
             <button className="App-botonAzul" style= {{top: "2%", left: "90%" , position:"absolute"}} onClick={() => cancelar()} >
               Volver
             </button>
           </div>
-
-          <div className="App-CuadradoGris" style={{ width: "100%", height: "85%", position: "absolute", top: "15%", left: "-0.2%"}}>
-            <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "5%"}}>
-                Nombre
-            </a>
-            <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "25%"}}>
-                Modo
-            </a>
-            <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "45%"}}>
-                Jugadores
-            </a>
-            <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "70%"}}>
-                Creador
-            </a>
-          </div>
           {show2 ? (
-          <div className="App-CuadradoBlanco" style={{ width: "100%", height: "73%", position: "absolute", top: "27%", left: "-0.2%"}}>
-          <InfiniteScroll
-            dataLength={salas.length}
-            pageStart={0}
-            loadMore={jugadores}
-            hasMore={true || false}
-            loader={<div className="loader" key={0}></div>}
-            useWindow={false}
-            style={{position:"absolute", width: "100%", height: "100%"}}
-          >
-            {jugadores()}
-          </InfiniteScroll>
-          </div>
+            <div>
+              <img src={Buscar} style={{width:"5%", height:"7%", left:"29%",zIndex: "1", top:"3%", position:"absolute"}}/>
+              <input className="App-textoNegro" label="pepe" name="pepe" onChange={handleChange3} value={searchTerm} style= {{top: "2%", left: "2%", position:"absolute", width: "500px", height: "50px"}} />
+              <button className="App-botonAzul" style= {{top: "2%", left: "74%" , position:"absolute"}} onClick={() => invitaciones()} >
+                Invitaciones
+              </button>
+              <div className="App-CuadradoGris" style={{ width: "100%", height: "85%", position: "absolute", top: "15%", left: "-0.2%"}}>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "5%"}}>
+                    Nombre
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "25%"}}>
+                    Modo
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "45%"}}>
+                    Jugadores
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "70%"}}>
+                    Creador
+                </a>
+              </div>
+              
+              <div className="App-CuadradoBlanco" style={{ width: "100%", height: "73%", position: "absolute", top: "27%", left: "-0.2%"}}>
+                <InfiniteScroll
+                  dataLength={salas.length}
+                  pageStart={0}
+                  loadMore={jugadores}
+                  hasMore={true || false}
+                  loader={<div className="loader" key={0}></div>}
+                  useWindow={false}
+                  style={{position:"absolute", width: "100%", height: "100%"}}
+                >
+                  {jugadores()}
+                </InfiniteScroll>
+              </div>
+            </div>
           ) : (
-            <div/>
+            <div>
+              <button className="App-botonAzul" style= {{top: "2%", left: "80%" , position:"absolute"}} onClick={() => {setShow2(true)}} >
+                Salas
+              </button>
+              <div className="App-CuadradoGris" style={{ width: "100%", height: "85%", position: "absolute", top: "15%", left: "-0.2%"}}>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "5%"}}>
+                    Nombre
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "25%"}}>
+                    Modo
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "45%"}}>
+                    Jugadores
+                </a>
+                <a  style= {{ color: "black", fontSize: "40px", fontStyle: "italic" ,position: "absolute", top: "1%", left: "70%"}}>
+                    Amigo
+                </a>
+              </div>
+              <div className="App-CuadradoBlanco" style={{ width: "100%", height: "73%", position: "absolute", top: "27%", left: "-0.2%"}}>
+                <InfiniteScroll
+                  dataLength={peticiones.length}
+                  pageStart={0}
+                  loadMore={invitaciones_lista}
+                  hasMore={true || false}
+                  loader={<div className="loader" key={0}></div>}
+                  useWindow={false}
+                  style={{position:"absolute", width: "100%", height: "100%"}}
+                >
+                  {invitaciones_lista()}
+                </InfiniteScroll>
+              </div>
+            
+              {show3 ? (
+                <div className="App-CuadradoAmarillo" style={{ width: "800px", height: "500px", position: "absolute", top: "20%", left: "30%"}}>
+                  <a  style= {{ color: "black", fontSize: "50px", fontStyle: "italic" ,position: "absolute", top: "8%", left: "35%"}}>
+                    Invitación Sala
+                  </a>
+                  <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "30%", left: "10%"}}>
+                    Nombre de la sala: {peticiones.me_invita}
+                  </a>
+                  <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "40%", left: "10%"}}>
+                    Modo de juego: {peticiones.me_invita}
+                  </a>
+                  <a  style= {{ color: "black", fontSize: "30px", fontStyle: "italic" ,position: "absolute", top: "50%", left: "10%"}}>
+                    Jugadores: {peticiones.me_invita}
+                  </a>
+                  <button className="App-botonConfirmar" style= {{top: "75%", left: "20%" , position:"absolute"}} onClick={funcionEntrar2} >
+                    Entrar
+                  </button>
+                  <button className="App-botonCancelar" style= {{top: "75%", left: "55%" , position:"absolute"}}onClick={() => setShow3(!show3)} >
+                    Cancelar
+                  </button>
+                </div>
+              ) : (
+                <div/>
+              )}
+              
+            </div>
           )}
-          a
         </div>
 
         ) : (
@@ -233,10 +351,13 @@ const ModoClasico = () => {
               </a>
             </div>
             <div>
-              <img src={Buscar} style={{width:"5%", height:"7%", left:"29%",zIndex: "1", top:"3%", cursor: "pointer", position:"absolute"}} onClick={cancelar}/>
-              <input className="App-textoNegro" label="pepe" name="pepe" onChange={handleChange} style= {{top: "2%", left: "2%", position:"absolute", width: "500px", height: "50px"}}/>
-              <button className="App-botonAzul" style= {{top: "2%", left: "85%" , position:"absolute"}}onClick={() => cancelar()} >
+              <img src={Buscar} style={{width:"5%", height:"7%", left:"29%",zIndex: "1", top:"3%", position:"absolute"}}/>
+              <input className="App-textoNegro" label="pepe" name="pepe" style= {{top: "2%", left: "2%", position:"absolute", width: "500px", height: "50px"}}/>
+              <button className="App-botonAzul" style= {{top: "2%", left: "90%" , position:"absolute"}} >
                 Volver
+              </button>
+              <button className="App-botonAzul" style= {{top: "2%", left: "74%" , position:"absolute"}} onClick={() => invitaciones()} >
+                Invitaciones
               </button>
             </div>
 
